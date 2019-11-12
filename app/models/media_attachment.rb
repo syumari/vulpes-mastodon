e1fd6d7fc29b63cdd6cb9c4c5d0d9dc240363bee
syumari <syumari@outlook.com>
2019-11-12 16:35:37 +0900

    Include Media Files capacity of media_attachment.rb

--- app/models/media_attachment.rb
+++ app/models/media_attachment.rb
@@ -42,7 +42,8 @@ class MediaAttachment < ApplicationRecord
 
   IMAGE_STYLES = {
     original: {
-      pixels: 1_638_400, # 1280x1280px
+      # pixels: 1_638_400, # 1280x1280px
+      pixels: 3_686_400,    # 1920x1920px
       file_geometry_parser: FastGeometryParser,
     },
 
@@ -88,7 +89,7 @@ class MediaAttachment < ApplicationRecord
         output: {
           'loglevel' => 'fatal',
           'map_metadata' => '-1',
-          'q:a' => 2,
+          'q:a' => 1,
         },
       },
     },
@@ -119,8 +120,8 @@ class MediaAttachment < ApplicationRecord
     original: VIDEO_FORMAT,
   }.freeze
 
-  IMAGE_LIMIT = 10.megabytes
-  VIDEO_LIMIT = 40.megabytes
+  IMAGE_LIMIT = 16.megabytes
+  VIDEO_LIMIT = 100.megabytes
 
   belongs_to :account,          inverse_of: :media_attachments, optional: true
   belongs_to :status,           inverse_of: :media_attachments, optional: true
